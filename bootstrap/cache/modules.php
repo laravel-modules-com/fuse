3828d6489ad1a96007fc149cd3ab7be5bdbdbcc5
David Carr <dave@dcblog.dev>
2025-03-16 01:22:52 +0000

    add configuration files for package and service providers

---
+++ bootstrap/cache/modules.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Admin\\Providers\\AdminServiceProvider',
+    1 => 'Modules\\AuditTrails\\Providers\\AuditTrailsServiceProvider',
+    2 => 'Modules\\Roles\\Providers\\RolesServiceProvider',
+    3 => 'Modules\\Settings\\Providers\\SettingsServiceProvider',
+    4 => 'Modules\\Users\\Providers\\UsersServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Admin\\Providers\\AdminServiceProvider',
+    1 => 'Modules\\AuditTrails\\Providers\\AuditTrailsServiceProvider',
+    2 => 'Modules\\Roles\\Providers\\RolesServiceProvider',
+    3 => 'Modules\\Settings\\Providers\\SettingsServiceProvider',
+    4 => 'Modules\\Users\\Providers\\UsersServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,134 @@
+<?php return array (
+  'barryvdh/laravel-ide-helper' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'kkomelin/laravel-translatable-string-exporter' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'KKomelin\\TranslatableStringExporter\\Providers\\ExporterServiceProvider',
+    ),
+  ),
+  'laracasts/flash' => 
+  array (
+    'aliases' => 
+    array (
+      'Flash' => 'Laracasts\\Flash\\Flash',
+    ),
+    'providers' => 
+    array (
+      0 => 'Laracasts\\Flash\\FlashServiceProvider',
+    ),
+  ),
+  'laravel/sail' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sail\\SailServiceProvider',
+    ),
+  ),
+  'laravel/sanctum' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'livewire/livewire' => 
+  array (
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+  ),
+  'mhmiton/laravel-modules-livewire' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Mhmiton\\LaravelModulesLivewire\\LaravelModulesLivewireServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'nwidart/laravel-modules' => 
+  array (
+    'aliases' => 
+    array (
+      'Module' => 'Nwidart\\Modules\\Facades\\Module',
+    ),
+    'providers' => 
+    array (
+      0 => 'Nwidart\\Modules\\LaravelModulesServiceProvider',
+    ),
+  ),
+  'pestphp/pest-plugin-laravel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Pest\\Laravel\\PestServiceProvider',
+    ),
+  ),
+  'spatie/laravel-ignition' => 
+  array (
+    'aliases' => 
+    array (
+      'Flare' => 'Spatie\\LaravelIgnition\\Facades\\Flare',
+    ),
+    'providers' => 
+    array (
+      0 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    ),
+  ),
+  'spatie/laravel-permission' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Permission\\PermissionServiceProvider',
+    ),
+  ),
+);
